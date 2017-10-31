03d28812dc32a54d3087a26e85f45bdc700e2aa8
fabulias <farid.abulias@gmail.com>
2017-10-31 01:01:08 -0300

    UI changes

--- src/pages/schedule/schedule.ts
+++ src/pages/schedule/schedule.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'page-schedule',
   templateUrl: 'schedule.html'
 })
 export class SchedulePage {
-
-  constructor(public navCtrl: NavController) {
+  public event = {
+    timeStarts1: '00:00',
+    timeStarts2: '00:00',
+    timeStarts3: '00:00',
+    timeStarts4: '00:00',
   }
-
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
+  }
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Espere un momento por favor...",
+      duration: 3000
+    });
+    loader.present();
+  }
 }
